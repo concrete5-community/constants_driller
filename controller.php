68b7f5dbab4c619e9da40b580ef17cac601b2aa3
Michele Locati <michele@locati.it>
2012-12-07 12:35:30 +0100

    Initial import

---
+++ controller.php
@@ -0,0 +1,41 @@
+<?php  defined('C5_EXECUTE') or die(_("Access Denied."));
+
+class ConstantsDrillerPackage extends Package {
+
+	protected $pkgHandle = 'constants_driller';
+	protected $appVersionRequired = '5.5';
+	protected $pkgVersion = '1.0';
+	
+	public function getPackageDescription() {
+		return t('Deep analysis and report of PHP constants used by concrete5.');
+	}
+	
+	public function getPackageName() {
+		return t('Constants Driller');
+	}
+
+	public static function addAlternateHrefLang($page) {
+		Loader::helper('interface/page', 'multilingual')->addAlternateHrefLang($page);
+	}
+
+	public function install() {
+		$pkg = parent::install();
+		$this->installOrUpgrade($pkg);
+	}
+
+	public function upgrade() {
+		$currentVersion = $this->getPackageVersion();
+		parent::upgrade();
+		$this->installOrUpgrade($this, $currentVersion);
+	}
+	
+	private function installOrUpgrade($pkg, $upgradeFromVersion = '') {
+		$fromScratch = strlen($upgradeFromVersion) ? false : true;
+		Loader::model('single_page');
+		if($fromScratch) {
+			$p = SinglePage::add('dashboard/system/environment/constants_driller',$pkg);
+		}
+		//if($fromScratch || version_compare($upgradeFromVersion, '1.0', '<'))
+	}
+
+}
